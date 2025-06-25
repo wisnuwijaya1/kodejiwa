5c87597cffed8017ac7192e9553682c48eeb3668
wisnu <nanuaja@gmail.com>
2025-06-25 14:40:51 +0700

    asd

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,11 +16,10 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name')->default('');
+            $table->string('tanggal')->default('');
             $table->string('email')->unique();
             $table->string('nphp')->default('');
             $table->string('status')->default('');
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
             $table->rememberToken();
             $table->timestamps();
         });
